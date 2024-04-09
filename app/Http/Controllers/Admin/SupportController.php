2c4d7151248dfe1c77976e85ad5c5e0eb03bda87
Mauriney Costa <maurineyy@gmail.com>
2024-04-09 16:29:39 -0300

    feat: support

---
+++ app/Http/Controllers/Admin/SupportController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\DTO\CreateSupportDTO;
+use App\DTO\UpdateSupportDTO;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
+use App\Models\Support;
+use App\Services\SupportService;
+use Illuminate\Http\Request;
+
+class SupportController extends Controller
+{
+    public function __construct(
+        protected SupportService $service
+    )
+    {}
+    public function index(Request $request)
+    {
+        $supports = $this->service->getAll($request->filter);
+
+        return view('admin/supports/index', compact('supports'));
+    }
+
+    public function show(string $id)
+    {
+        // $support = Support::find($id)
+        // $support = Support::where('id', $id)->first()
+        // $support = Support::where('id', '!=', $id)->first()
+        if (!$support = $this->service->findOne($id)) {
+            return back();
+        }
+        // dd($support);
+        return view('admin/supports/show', compact('support'));
+    }
+
+    public function create()
+    {
+        return view('admin/supports/create');
+    }
+
+    public function store(StoreUpdateSupport $request, Support $support)
+    {
+        $this->service->new(
+            CreateSupportDTO::makeFromRequest($request)
+        );
+
+        return redirect()->route('supports.index');
+    }
+
+    public function edit(string $id)
+    {
+        // if (!$support = $support::where('id', $id)->first()) {
+        if (!$support = $this->service->findOne($id)) {
+            return back();
+        } 
+
+        return view('admin/supports.edit', compact('support'));
+    }
+
+    public function update(StoreUpdateSupport $request, Support $support, string $id)
+    {
+        $support = $this->service->update(
+            UpdateSupportDTO::makeFromRequest($request)
+        );
+        if (!$support) {
+            return back();
+        }
+
+        return redirect()->route('supports.index');
+    }
+
+    public function destroy(string|int $id) 
+    {
+        $this->service->delete($id);
+        
+        return redirect()->route('supports.index');
+    }
+}
+
+
+// namespace App\Http\Controllers\Admin;
+
+// use App\Http\Controllers\Controller;
+// use App\Http\Requests\StoreUpdateSupport;
+// use App\Models\Support;
+// use Illuminate\Http\Request;
+
+// class SupportController extends Controller
+// {
+//     public function index(Support $support)
+//     {
+//         $supports = $support->all();
+
+//         return view('admin/supports/index', compact('supports'));
+//     }
+
+//     public function show(string|int $id)
+//     {
+//         // $support = Support::find($id)
+//         // $support = Support::where('id', $id)->first()
+//         // $support = Support::where('id', '!=', $id)->first()
+//         if (!$support = Support::find($id)) {
+//             return back();
+//         }
+//         // dd($support);
+//         return view('admin/supports/show', compact('support'));
+//     }
+
+//     public function create()
+//     {
+//         return view('admin/supports/create');
+//     }
+
+//     public function store(StoreUpdateSupport $request, Support $support)
+//     {
+//         // dd($request->all());
+//         // $data = $request->All();
+//         $data = $request->validated();
+//         $data['status'] = 'a';
+
+//         $support->create($data);
+
+//         return redirect()->route('supports.index');
+//     }
+
+//     public function edit(Support $support, string|int $id)
+//     {
+//         if (!$support = $support::where('id', $id)->first()) {
+//             return back();
+//         } 
+
+//         return view('admin/supports.edit', compact('support'));
+//     }
+
+//     public function update(StoreUpdateSupport $request, Support $support, string $id)
+//     {
+//         if (!$support = $support::find($id)) {
+//             return back();
+//         }
+
+//         // $support->subject = $request->subject;
+//         // $support->body = $request->body;
+//         // $support->save(); Outra forma de salvar, serve para salvar e editar
+
+//         // $support->update($request->only([
+//         //     'subject', 'body'
+//         // ]));
+//         $support->update($request->validated());
+
+//         return redirect()->route('supports.index');
+//     }
+
+//     public function destroy(string|int $id) 
+//     {
+//         if (!$support = Support::find($id)) {
+//             return back();
+//         }
+
+//         $support->delete();
+        
+//         return redirect()->route('supports.index');
+//     }
+// }
